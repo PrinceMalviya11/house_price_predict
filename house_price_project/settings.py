a9b2c94b93abc0f82a5b993778736267115f8c07
Prince Malviya <princemalviya349@gmail.com>
2026-01-01 09:06:22 +0530

    Update email settings to use placeholders
    Replaced email credentials with placeholders for security.

--- house_price_project/settings.py
+++ house_price_project/settings.py
@@ -129,6 +129,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'princemalaviya11@gmail.com'
-EMAIL_HOST_PASSWORD = 'ogrf mvcx ekpj jftr'
+EMAIL_HOST_USER = 'YOUR EMAIL ADDRESS'
+EMAIL_HOST_PASSWORD = 'YOUR APP PASSKEY'
 
