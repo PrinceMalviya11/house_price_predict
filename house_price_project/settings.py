4b91f1a9d2ba1b746f2fe54ef8a40c7b940bc21c
Prince Malviya <princemalviya349@gmail.com>
2026-01-05 19:43:11 +0530

    Remove email backend configuration
    Removed email configuration settings from settings.py.

--- house_price_project/settings.py
+++ house_price_project/settings.py
@@ -122,13 +122,4 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-# Email Address add
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-
-EMAIL_HOST_USER = 'YOUR EMAIL ADDRESS'
-EMAIL_HOST_PASSWORD = 'YOUR APP PASSKEY'
 
